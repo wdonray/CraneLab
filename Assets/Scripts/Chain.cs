1b101951787a9af52fb80cf0bd81c35ffbfe8092
Eric Mouledoux <EricMouledoux@gmail.com>
2018-05-14 15:34:21 -0500

    metas

---
+++ Assets/Scripts/Chain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chain : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_chainLinkPrefab;
+
+    private Stack<GameObject> m_chainLinks;
+
+    private void AddLink()
+    {
+
+    }
+}
