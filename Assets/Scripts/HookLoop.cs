8572ddf7e47022996a961a87b5aee46bad7c8b58
Eric Mouledoux <EricMouledoux@gmail.com>
2018-06-14 08:48:52 -0500

    Hook loop demo

--- Assets/Scripts/HookLoop.cs
+++ Assets/Scripts/HookLoop.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class HookLoop : MonoBehaviour
 {
+    Transform m_hook;
+
+    Vector3 m_lastPos;
+    Vector3 m_velocity;
+
 	// Use this for initialization
 	void Start ()
     {
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        m_lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
+        if (m_hook != null)
+        {
+            Vector3 d2h = m_hook.position - transform.position;
+            d2h *= Time.deltaTime;
+            transform.position = (m_hook.position);
+            transform.rotation = m_hook.rotation;
 
-	}
+            if (Input.GetButtonDown("RIGHT_TRIGGER"))
+            {
+                Drop();
+            }
+        }
+    }
 
+    private void FixedUpdate()
+    {
+        m_velocity = transform.position - m_lastPos;
+        m_lastPos = transform.position;
+
+        print(m_velocity);
+    }
+
+    public void Drop()
+    {
+        m_hook = null;
+        GetComponent<Rigidbody>().velocity = m_velocity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (m_hook != null) return;
+
+
+        else
+        {
+        print("hook");
+            if(other.CompareTag("Hook"))
+            {
+                m_hook = other.transform;
+            }
+        }
+    }
 }
--- Assets/Scripts/ObiRopeControls.cs
+++ Assets/Scripts/ObiRopeControls.cs
@@ -7,19 +7,29 @@ using Obi;
 public class ObiRopeControls : MonoBehaviour
 {
     public string axis;
+    public float reelSpeed;
 
     ObiRope rope;
     ObiRopeCursor cursor;
+
+    float ropeLength;
 
     // Use this for initialization
     void Start()
     {
         cursor = GetComponent<ObiRopeCursor>();
         rope = cursor.GetComponent<ObiRope>();
+
+        ropeLength = rope.RestLength;
     }
 
     void Update()
     {
-        cursor.ChangeLength(rope.RestLength + -Input.GetAxis(axis) * Time.deltaTime);
+        if (Input.GetAxis(axis) != 0f)
+        {
+            ropeLength -= Input.GetAxis(axis) * reelSpeed * Time.deltaTime;
+            cursor.ChangeLength(ropeLength);
+            //cursor.normalizedCoord = 0.8f;
+        }
 	}
 }
