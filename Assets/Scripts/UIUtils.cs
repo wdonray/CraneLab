765780c1a657d0f96ae4aa237d029c03fde0baff
Eric Mouledoux <EricMouledoux@gmail.com>
2018-06-19 15:51:47 -0500

    Newton physics

---
+++ Assets/Scripts/UIUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIUtils : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            ReloadCurrentScene();
+    }
+
+    public void GoToScene(int index)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(index);
+    }
+
+    public void GoToScene(string name)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(name);
+    }
+
+    public void ReloadCurrentScene()
+    {
+        GoToScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
