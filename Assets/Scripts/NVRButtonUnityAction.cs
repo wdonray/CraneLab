675e69d1365ba567ad47eed959f5f994d9da53b2
Eric Mouledoux <EricMouledoux@gmail.com>
2018-06-20 09:11:18 -0500

    button vr

---
+++ Assets/Scripts/NVRButtonUnityAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NVRButtonUnityAction : MonoBehaviour
+{
+    public NewtonVR.NVRButton Button;
+    public UnityEngine.Events.UnityEvent UnityEvent;
+
+    private void Start()
+    {
+        if (Button == null)
+            Button = GetComponent<NewtonVR.NVRButton>();
+
+        if (Button == null) Destroy(this);
+    }
+
+    private void Update()
+    {
+        if (Button.ButtonDown)
+        {
+            UnityEvent.Invoke();
+        }
+    }
+}
