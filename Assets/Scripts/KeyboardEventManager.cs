8a4e74d9882025c55b473670234b179c65dcaf02
Eric Mouledoux <EricMouledoux@gmail.com>
2018-06-22 14:14:26 -0500

    script for tep admin controls

---
+++ Assets/Scripts/KeyboardEventManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardEventManager : MonoBehaviour
+{
+    private List<KeyboardEvent> m_keyEvents = new List<KeyboardEvent>();
+	
+
+	void Update ()
+    {
+		
+	}
+}
+
+[System.Serializable]
+public sealed class KeyboardEvent
+{
+    public KeyCode m_key;
+    public UnityEngine.Events.UnityEvent m_event;
+}
