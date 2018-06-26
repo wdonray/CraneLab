1a4b9df0a763e83d8e44367a708da264a5eeb4b8
Eric Mouledoux <EricMouledoux@gmail.com>
2018-06-26 11:05:05 -0500

    temp rigger arrow

--- Assets/LoadGuidance.cs
+++ Assets/LoadGuidance.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class LoadGuidance : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
+    public Transform m_load;
+    public Transform m_dropZone;
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        transform.LookAt(CalculateWorldPointPosition());
 	}
+
+    private Vector3 CalculateWorldPointPosition()
+    {
+        if (m_load == null || m_dropZone == null) return Vector3.zero;
+
+        Vector3 targetWorldPos = Vector3.zero;
+
+        targetWorldPos = m_dropZone.position - m_load.position;
+        targetWorldPos.Normalize();
+        targetWorldPos += transform.position;
+
+        return targetWorldPos;
+    }
 }
