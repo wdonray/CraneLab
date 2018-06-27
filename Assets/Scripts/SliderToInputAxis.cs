b2c99707dcf06bbb44cad254d56befb907a43f01
Eric Mouledoux <EricMouledoux@gmail.com>
2018-06-27 14:41:24 -0500

    prefab for demo bbox

---
+++ Assets/Scripts/SliderToInputAxis.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class SliderToInputAxis : MonoBehaviour
+{
+    private Slider m_slider;
+
+    [SerializeField]
+    private string m_axis;
+    [SerializeField]
+    private bool m_absolute;
+
+
+
+	void Start ()
+    {
+        m_slider = GetComponent<Slider>();
+	}
+	
+    
+	void Update ()
+    {
+        float value = Input.GetAxis(m_axis);
+        value = m_absolute ? Mathf.Abs(value) : value;
+        m_slider.value = value;
+	}
+}
