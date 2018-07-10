ad6c2151062338bf89bd4e35be905240b14722e7
Eric Mouledoux <EricMouledoux@gmail.com>
2018-07-10 12:07:03 -0500

    Occlusion

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time;
+    public List<Text> timerText = new List<Text>();
+
+    public UnityEngine.Events.UnityEvent onTimerEnd;
+
+    [Space]
+
+    public bool hasTriggered = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (hasTriggered) return;
+
+        time -= Time.deltaTime;
+
+        foreach (Text t in timerText)
+        {
+            t.text = Mathf.Floor(time / 60f).ToString("00") + ":" + (time % 60f).ToString("00");
+        }
+
+        if (time <= 0f)
+        {
+            onTimerEnd.Invoke();
+            hasTriggered = true;
+        }
+	}
+}
