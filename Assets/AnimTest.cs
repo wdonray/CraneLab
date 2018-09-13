3b306e6c388608c04c7cd3d1304aab5df706975d
Eric Mouledoux <EricMouledoux@gmail.com>
2018-09-13 09:10:08 -0500

    animation testing video

---
+++ Assets/AnimTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour
+{
+    public Animator m_animator;
+    public string m_trigger;
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_animator.SetTrigger(m_trigger);
+        }
+	}
+}
