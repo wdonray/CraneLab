f8524e097b9a6a25b0b271c9c7c6b6bde12d6ad1
Eric Mouledoux <EricMouledoux@gmail.com>
2018-09-14 14:33:11 -0500

    New scene for propper scale sandbox

---
+++ Assets/SkyboxRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour
+{
+    public Material skyboxMaterial;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
