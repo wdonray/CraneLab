9e44582dae55db342e3c7bee00597ed7abc666a6
Eric Mouledoux <EricMouledoux@gmail.com>
2018-09-18 17:46:15 -0500

    barrel pallet

---
+++ Assets/SoundOnImpact.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnImpact : MonoBehaviour
+{
+    public float forceThreshold;
+    public AudioSource audioSource;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > forceThreshold)
+            audioSource.PlayOneShot(audioSource.clip);
+    }
+}
