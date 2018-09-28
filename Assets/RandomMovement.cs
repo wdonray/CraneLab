c5df9d67a073ed7dff3594344e63225faf4faef0
Eric Mouledoux <EricMouledoux@gmail.com>
2018-09-28 11:12:25 -0500

    boat 4d movement

--- Assets/RandomMovement.cs
+++ Assets/RandomMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RandomMovement : MonoBehaviour
 {
-    public UnityEngine.UI.Slider noise;
+    public UnityEngine.UI.Slider strengthSlider;
 
     public Vector3 initPosition;
     public Vector3 randomPos;
@@ -23,6 +23,14 @@ public class RandomMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.forward, 1f * speed);
+        Vector3 newPos = initPosition;
+        newPos.y += Mathf.Cos(Time.time) * strengthSlider.value;
+
+        Vector3 newEulers = new Vector3(0.1f, 1f, 5f) * Mathf.Sin(Time.time) * strengthSlider.value;
+        Quaternion newRot = Quaternion.Euler(newEulers);
+
+        transform.localPosition = newPos;
+        transform.localRotation = newRot;
+        
     }
 }
