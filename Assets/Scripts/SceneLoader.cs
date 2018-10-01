eb08a258dbf3f0e100f2c7d41812df8ef3878b5a
Eric Mouledoux <EricMouledoux@gmail.com>
2018-10-01 12:44:21 -0500

    static

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,13 +6,27 @@ public class SceneLoader : MonoBehaviour
 {
     public static string onLoadMessage;
 
-    public void SetLoadMessage(string newMessage)
+    private static SceneLoader _instance;
+    public static SceneLoader instance
+    {
+        get
+        {
+            if (_instance = null) FindObjectOfType<SceneLoader>();
+            return _instance;
+        }
+    }
+
+
+    public static void SetLoadMessage(string newMessage)
     {
         onLoadMessage = newMessage;
     }
 
+
     private void Awake()
     {
+        if (instance != this) Destroy(this);
+
         if (onLoadMessage == null) onLoadMessage = "";
         Mouledoux.Components.Mediator.instance.NotifySubscribers(onLoadMessage, new Mouledoux.Callback.Packet());
         onLoadMessage = null;
