6bf0b8e4ccc57986a92b4693b716bbbcf3a4f4fd
Eric Mouledoux <EricMouledoux@gmail.com>
2018-10-01 12:35:08 -0500

    scene loader w/ messagin system

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static string onLoadMessage;
+
+    public void SetLoadMessage(string newMessage)
+    {
+        onLoadMessage = newMessage;
+    }
+
+    private void Awake()
+    {
+        if (onLoadMessage == null) onLoadMessage = "";
+        Mouledoux.Components.Mediator.instance.NotifySubscribers(onLoadMessage, new Mouledoux.Callback.Packet());
+        onLoadMessage = null;
+    }
+}
