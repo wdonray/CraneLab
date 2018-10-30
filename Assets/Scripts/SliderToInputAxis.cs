2d14819802f3aa48c90375af7f1928fd1328f0f4
Eric Mouledoux <EricMouledoux@gmail.com>
2018-10-30 14:59:09 -0500

    generator drop bug fix

--- Assets/Scripts/SliderToInputAxis.cs
+++ Assets/Scripts/SliderToInputAxis.cs
@@ -9,22 +9,54 @@ public class SliderToInputAxis : MonoBehaviour
     private Slider m_slider;
 
     [SerializeField]
+    private Text m_text;
+
+    [SerializeField]
     private string m_axis;
     [SerializeField]
     private bool m_absolute;
 
+    private float value
+    {
+        get
+        {
+            float n = Input.GetAxis(m_axis);
+            //n = m_absolute ? Mathf.Abs(value) : n;
+            return n;
+        }
+    }
+    private float valueRaw
+    {
+        get
+        {
+            float n = Input.GetAxisRaw(m_axis);
+            return n;
+        }
+    }
 
-
-	void Start ()
+    void Start ()
     {
         m_slider = GetComponent<Slider>();
-	}
-	
-    
-	void Update ()
+    }
+
+    private void Update()
     {
-        float value = Input.GetAxis(m_axis);
-        value = m_absolute ? Mathf.Abs(value) : value;
-        m_slider.value = value;
-	}
+        if (m_slider != null)
+        {
+            m_slider.value = value;
+        }
+    }
+
+    //private void OnGUI()
+    //{
+    //    if (m_slider != null)
+    //    {
+    //        m_slider.value = value;
+    //    }
+
+    //    if(m_text != null)
+    //    {
+    //        m_text.text = ((-valueRaw + 1f) * 0.5f).ToString();
+    //    }
+    //}
 }
