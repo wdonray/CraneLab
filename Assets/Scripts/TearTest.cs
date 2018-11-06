d625e652f7a73285e3e0335d6b36743d2c9901f7
Donray <w.donray@yahoo.com>
2018-11-06 10:56:59 -0600

    Working on breaking rope

--- Assets/Scripts/TearTest.cs
+++ Assets/Scripts/TearTest.cs
@@ -1,24 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Leap.Unity;
 using Obi;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Events;
 using ObiRope = Obi.ObiRope;
+using Random = UnityEngine.Random;
 
 public class TearTest : MonoBehaviour
 {
+    public float Minimum, Maximum;
+    public float TimePicked;
     public float DistanceToTrigger;
     private Vector3 _startPos, _currentPos;
-    private List<ObiRope> _ropes = new List<ObiRope>();
+    [SerializeField] private List<ObiRope> _ropes = new List<ObiRope>();
     public ObiRope Rope;
     private IEnumerator _coroutine;
-    public int TimePicked;
-    private bool _running;
 
+    private bool _running;
+    public static bool _failed, _passed;
+    private float minTemp, maxTemp;
     // Use this for initialization
     void Awake()
     {
-        _coroutine = StartBreakEvent(30, 120, () => Break(2));
+        _coroutine = StartBreakEvent(Minimum, Maximum, () => Break(2));
         foreach (var obi in GetComponentsInChildren<ObiRope>())
         {
             _ropes.Add(obi);
@@ -35,44 +42,74 @@ public class TearTest : MonoBehaviour
 
     public void StartBreakCoroutine()
     {
-        if (_running == false)
-        {
-            StartCoroutine(_coroutine);
-        }
+        StartCoroutine(_coroutine);
     }
 
     public void StopBreakCoroutine()
     {
-        if (_running == true)
-        {
-            StopCoroutine(_coroutine);
-        }
+        StopCoroutine(_coroutine);
     }
 
-    public IEnumerator StartBreakEvent(int minimum, int maximum, UnityAction action)
+    public IEnumerator StartBreakEvent(float minimum, float maximum, UnityAction action)
     {
-        _running = true;
         TimePicked = Random.Range(minimum, maximum);
         yield return new WaitForSeconds(TimePicked);
-        yield return new WaitUntil(Check);
+        Debug.Log("Time reached for " + transform.name);
+        yield return new WaitUntil(CheckDistanceAway);
+        if (!_failed && _running == false)
+        {
+            StartCoroutine(PlacedDown());
+        }
+        Debug.Log("Distance reached for " + transform.name);
         action.Invoke();
         Debug.Log(transform.name + ", " + Rope + " broke!");
-        _running = false;
     }
 
     private void Break(int ropeIndex)
     {
-        Rope.DistanceConstraints.RemoveFromSolver(null);
-        Rope.BendingConstraints.RemoveFromSolver(null);
+        if (_passed == false)
+        {
+            Rope.DistanceConstraints.RemoveFromSolver(null);
+            Rope.BendingConstraints.RemoveFromSolver(null);
 
-        Rope.Tear(ropeIndex);
+            Rope.Tear(ropeIndex);
 
-        Rope.BendingConstraints.AddToSolver(this);
-        Rope.DistanceConstraints.AddToSolver(this);
+            Rope.BendingConstraints.AddToSolver(this);
+            Rope.DistanceConstraints.AddToSolver(this);
 
-        Rope.BendingConstraints.SetActiveConstraints();
+            Rope.BendingConstraints.SetActiveConstraints();
 
-        Rope.Solver.UpdateActiveParticles();
+            Rope.Solver.UpdateActiveParticles();
+
+            _ropes.Remove(Rope);
+            if (_ropes.Count > 0)
+            {
+                Rope = _ropes[Random.Range(0, _ropes.Count)];
+                switch (_ropes.Count)
+                {
+                    case 3:
+                        {
+                            minTemp = Minimum / 3.5f;
+                            maxTemp = Maximum / 4;
+                        }
+                        break;
+                    default:
+                        {
+                            minTemp /= 3.5f;
+                            maxTemp /= 4;
+                        }
+                        break;
+                }
+
+                _coroutine = StartBreakEvent(minTemp, maxTemp, () => Break(2));
+                StartBreakCoroutine();
+            }
+            else
+            {
+                _failed = true;
+                Debug.Log("You failed!");
+            }
+        }
     }
 
     void OnDestroy()
@@ -80,9 +117,26 @@ public class TearTest : MonoBehaviour
         StopBreakCoroutine();
     }
 
-    private bool Check()
+    private bool CheckDistanceAway()
     {
         var dist = Vector3.Distance(_startPos, _currentPos);
         return (dist > DistanceToTrigger);
+    }
+
+    private bool CheckIfStopped()
+    {
+        return transform.GetChild(2).GetComponent<Rigidbody>().velocity.magnitude <= 0.1f;
+    }
+
+    private IEnumerator PlacedDown()
+    {
+        _running = true;
+        yield return new WaitForSeconds(0.3f);
+        yield return new WaitUntil(CheckIfStopped);
+        if (_failed == false)
+        {
+            _passed = true;
+            Debug.Log("You Passed!");
+        }
     }
 }
