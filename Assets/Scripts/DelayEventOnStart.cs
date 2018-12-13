cc5d1aec183e2070fd4abe9fc950f57512ceb5fb
Donray <w.donray@yahoo.com>
2018-12-13 10:39:22 -0600

    Delay Event

---
+++ Assets/Scripts/DelayEventOnStart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayEventOnStart : MonoBehaviour
+{
+    public float Delay;
+    public UnityEngine.Events.UnityEvent _action;
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds(Delay);
+        _action.Invoke();
+        Destroy(this);
+	}
+}
